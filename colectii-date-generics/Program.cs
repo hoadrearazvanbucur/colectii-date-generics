f9f95078f9371f314ec2798843a76c130a6facbe
hoadrearazvanbucur <hoadrea.razvan@yahoo.com>
2021-11-20 18:16:37 +0200

    .

---
+++ colectii-date-generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace colectii_date_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
